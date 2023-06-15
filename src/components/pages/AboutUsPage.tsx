09996ef195716386f3eb3878274f56721a807303
Tuan Nguyen <tuan.nguyen@c0x12c.com>
2023-06-15 15:12:50 +0700

    feat: implement stand out section

--- src/components/pages/AboutUsPage.tsx
+++ src/components/pages/AboutUsPage.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
 import MainTemplate from '../templates/MainTemplate';
+import { Box } from '@mui/material';
 import AboutUs from '../molecules/AboutUs';
+import StandOut from '../molecules/StandOut';
 
 interface IAboutUsPageProps {}
 
 const AboutUsPage: React.FunctionComponent<IAboutUsPageProps> = (props) => {
   return (
     <MainTemplate>
-      <AboutUs />
+      <Box
+        sx={{
+          background:
+            'linear-gradient(180deg, #FFFFFF 8.48%, rgba(222, 229, 255, 0.739633) 49.33%, rgba(255, 255, 255, 0) 100%);',
+        }}
+      >
+        <AboutUs />
+        <StandOut />
+      </Box>
     </MainTemplate>
   );
 };
