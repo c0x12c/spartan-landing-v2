4d11899797aa8fd56dd4ff6bbaf5aace5456cf18
Tuan Nguyen <tuan.nguyen@c0x12c.com>
2023-06-17 14:55:38 +0700

    feat: implement case study show all projects

--- src/components/pages/CaseStudyPage.tsx
+++ src/components/pages/CaseStudyPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MainTemplate from '../templates/MainTemplate';
 import CaseStudyBanner from '../molecules/CaseStudy/Banner';
+import Cases from '../molecules/CaseStudy/Cases';
 
 type Props = {
   children?: React.ReactNode;
@@ -10,6 +11,7 @@ const CaseStudyPage: React.FC<Props> = () => {
   return (
     <MainTemplate>
       <CaseStudyBanner />
+      <Cases />
     </MainTemplate>
   );
 };
