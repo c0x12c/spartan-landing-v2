f123e9c97bd13f8c25f92c7b5444241ebde6c23b
Duc Duong <duc.duong@c0x12c.com>
2023-06-20 21:27:48 +0700

    chore: remove export config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
