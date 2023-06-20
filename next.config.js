81b44583fca62cbfbf97dea5280770f4385170a2
Tuan Nguyen <tuan.nguyen@c0x12c.com>
2023-06-20 15:50:47 +0700

    chore: update trailingSlash + export mode in config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
