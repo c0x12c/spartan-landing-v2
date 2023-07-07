5c0b212e923a14651305b17f0214e19adefe6ac2
Tuan Nguyen <tuan.nguyen@c0x12c.com>
2023-07-07 16:26:27 +0700

    feat: implement submenu for header

--- src/components/atoms/common/MenuItemDesktop.tsx
+++ src/components/atoms/common/MenuItemDesktop.tsx
@@ -35,12 +35,12 @@ export const MenuItemDesktop = ({ item, isTransparent }: MenuItemDesktopType) =>
             color: primary[500],
             transition: 'color .5s',
           },
+          '& .menu-title::before': {
+            width: '100%',
+          },
           '& .icon-expand': {
             color: primary[500],
             transition: 'color .5s',
-          },
-          '&::before': {
-            width: '100%',
           },
           '& .submenu': {
             opacity: 1,
@@ -48,21 +48,29 @@ export const MenuItemDesktop = ({ item, isTransparent }: MenuItemDesktopType) =>
             visibility: 'visible',
           },
         },
-        '&::before': {
-          content: '""',
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          width: 0,
-          height: '2px',
-          backgroundColor: primary[500],
-          transition: 'width .5s',
-        },
       }}
       px="10.63px"
-      position="relative"
+      position={item.label !== 'Services' ? 'relative' : 'unset'}
     >
-      <Typography className="menu-title" variant="fs18" color={color} lineHeight={1.33}>
+      <Typography
+        className="menu-title"
+        variant="fs18"
+        color={color}
+        lineHeight={1.33}
+        position="relative"
+        sx={{
+          '&::before': {
+            content: '""',
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            width: 0,
+            height: '2px',
+            backgroundColor: primary[500],
+            transition: 'width .5s',
+          },
+        }}
+      >
         {item.label}
       </Typography>
       {item.subItems && (
@@ -73,72 +81,82 @@ export const MenuItemDesktop = ({ item, isTransparent }: MenuItemDesktopType) =>
           }}
         />
       )}
-      {item.subItems && (
-        <Box
-          className="submenu"
-          component="ul"
-          position="absolute"
-          top="120%"
-          left="-100%"
-          bgcolor={base.white}
-          p={'20px 32px 32px'}
-          width={'417px'}
-          sx={{
-            visibility: 'hidden',
-            opacity: 0,
-            transition: 'all .5s',
-          }}
-        >
-          {(item.subItems ?? []).map((subItem, subIndex) => {
-            const { href, hash } = subItem as ServiceType & AboutType;
-            const link = href ? href : `/services#${hash}`;
-            return (
+      <SubMenu item={item} />
+    </Box>
+  );
+};
+
+const SubMenu = ({ item }: Pick<MenuItemDesktopType, 'item'>) => {
+  if (!item.subItems) {
+    return;
+  }
+
+  const isServices = item.label === 'Services';
+  return (
+    <Box
+      className={`submenu ${item.label.toLowerCase()}`}
+      component="ul"
+      position="absolute"
+      top="120%"
+      left={isServices ? 0 : '-100%'}
+      right={isServices ? 0 : 'unset'}
+      bgcolor={base.white}
+      p={'20px 32px 32px'}
+      width={isServices ? '90vw' : '417px'}
+      maxWidth={isServices ? '1208px' : '100%'}
+      sx={{
+        visibility: 'hidden',
+        opacity: 0,
+        transition: 'all .5s',
+      }}
+    >
+      {(item.subItems ?? []).map((subItem, subIndex) => {
+        const { href } = subItem;
+        return (
+          <Box
+            key={subItem.id}
+            component="li"
+            borderBottom="1px solid"
+            borderColor={gray[400]}
+            p="4px 10px 8px"
+            mb={subIndex === (item.subItems || []).length - 1 ? 0 : '17px'}
+            sx={{
+              '&:hover': {
+                '& .submenu-title': {
+                  color: primary[500],
+                  transition: 'color .5s',
+                },
+                '& .submenu-icon': {
+                  transform: 'translateX(5px)',
+                  transition: 'transform .5s',
+                },
+              },
+            }}
+          >
+            <Link href={href} scroll={false}>
               <Box
-                key={subItem.id}
-                component="li"
-                borderBottom="1px solid"
-                borderColor={gray[400]}
-                p="4px 10px 8px"
-                mb={subIndex === (item.subItems || []).length - 1 ? 0 : '17px'}
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
                 sx={{
-                  '&:hover': {
-                    '& .submenu-title': {
-                      color: primary[500],
-                      transition: 'color .5s',
-                    },
-                    '& .submenu-icon': {
-                      transform: 'translateX(5px)',
-                      transition: 'transform .5s',
-                    },
-                  },
+                  textDecoration: 'none',
                 }}
               >
-                <Link href={link} scroll={false}>
-                  <Box
-                    display="flex"
-                    justifyContent="space-between"
-                    alignItems="center"
-                    sx={{
-                      textDecoration: 'none',
-                    }}
-                  >
-                    <Typography
-                      className="submenu-title"
-                      variant="fs16"
-                      sx={{ color: base.black, fontWeight: 700, paddingRight: '30px' }}
-                    >
-                      {subItem.label}
-                    </Typography>
-                    <Box className="submenu-icon">
-                      <ArrowIcon color={base.black} />
-                    </Box>
-                  </Box>
-                </Link>
+                <Typography
+                  className="submenu-title"
+                  variant="fs16"
+                  sx={{ color: base.black, fontWeight: 700, paddingRight: '30px' }}
+                >
+                  {subItem.label}
+                </Typography>
+                <Box className="submenu-icon">
+                  <ArrowIcon color={base.black} />
+                </Box>
               </Box>
-            );
-          })}
-        </Box>
-      )}
+            </Link>
+          </Box>
+        );
+      })}
     </Box>
   );
 };
--- src/constants/services.ts
+++ src/constants/services.ts
@@ -1,58 +1,60 @@
+import mobileIcon from '@/assets/images/icons/mobile.svg';
+import webIcon from '@/assets/images/icons/web.svg';
+import backendIcon from '@/assets/images/icons/backend.svg';
+import cloudIcon from '@/assets/images/icons/cloud.svg';
+import emergingIcon from '@/assets/images/icons/emerging.svg';
+import questionIcon from '@/assets/images/icons/question.svg';
+import { StaticImageData } from 'next/image';
+
 export type ServiceType = {
   id: string;
   label: string;
-  skills: string[];
-  hash: string;
+  href: string;
   content: string;
+  icon: StaticImageData;
 };
 
 export const Services = [
   {
     id: 'service-1',
-    label: 'IT Consulting',
-    skills: ['Research & Development', 'Rapid prototyping', 'Scoping Session'],
-    hash: 'it-consulting',
-    content:
-      'Spartans is renowned for its affordable, reliable, and expert IT consulting services. We provide top-of-the-line technologies, software, and applications that help enterprises gain a competitive edge in the market.',
+    label: 'Mobile App Development',
+    href: '/services/mobile-app',
+    content: 'We help enterprises navigating the best of the digital',
+    icon: mobileIcon,
   },
   {
     id: 'service-2',
-    label: 'Product Development',
-    skills: ['Research & Development', 'Rapid prototyping', 'Scoping Session'],
-    hash: 'product-development',
-    content:
-      'We help enterprises navigating the best of the digital landscapes through our outsourced product development services that enable you to deliver highly productive outcomes.',
+    label: 'Web Development',
+    href: '/services/web-development',
+    content: 'We help enterprises navigating the best of the digital',
+    icon: webIcon,
   },
   {
     id: 'service-3',
-    label: 'Artificial Intelligence',
-    skills: ['Research & Development', 'Rapid prototyping', 'Scoping Session'],
-    hash: 'artificial-intelligence',
-    content:
-      "We offer all types and sizes of enterprises to leverage our 11+ years' expertise and experience, as we undoubtedly provide the best and the most reliable AI development services in the market.",
+    label: 'Backend Development',
+    href: '/services/backend',
+    content: 'We help enterprises navigating the best of the digital',
+    icon: backendIcon,
   },
   {
     id: 'service-4',
-    label: 'Data Platform',
-    skills: ['Research & Development', 'Rapid prototyping', 'Scoping Session'],
-    hash: 'data-platform',
-    content:
-      'Our comprehensive Data Platform service offers end-to-end solutions that cover everything from designing data architecture to developing pipelines, integrating data, establishing governance, warehousing, and conducting analytics. Our goal is to enable organizations to enhance their data quality, reliability, and accessibility, which can lead to improved business outcomes.',
+    label: 'Cloud Solutions',
+    href: '/services/cloud',
+    content: 'We help enterprises navigating the best of the digital',
+    icon: cloudIcon,
   },
   {
     id: 'service-5',
-    label: 'Quality Assurance (QA) & Testing',
-    skills: ['Research & Development', 'Rapid prototyping', 'Scoping Session'],
-    hash: 'qa-testing',
-    content:
-      'We provide trusted, experts QA, and software testing services for mobile and web apps by using best practices and automated & manual testing methods.',
+    label: 'Emerging Technologies',
+    href: '/services/emerging-technologies',
+    content: 'We help enterprises navigating the best of the digital',
+    icon: emergingIcon,
   },
   {
     id: 'service-6',
-    label: 'IoT Development',
-    skills: ['Research & Development', 'Rapid prototyping', 'Scoping Session'],
-    hash: 'iot-development',
-    content:
-      'Leverage our immense experience in IoT to launch a new IoT implementation, or to upgrade and integrate the existing ones most reliably and cost-effectively.',
+    label: 'How Spartan operates',
+    href: '/spartan-operates',
+    content: 'We help enterprises navigating the best of the digital',
+    icon: questionIcon,
   },
 ];
