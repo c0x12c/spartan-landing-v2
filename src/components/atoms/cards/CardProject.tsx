64b0d0eed5d55cc9ac3b0f9f9a24da33750b6d55
Tuan Nguyen <121297123+spartan-tuannguyen@users.noreply.github.com>
2023-07-12 15:39:04 +0700

    Update style Case Study page (#34)
    * fix: update image descripton section (about-us)
    
    * style: update style for card vertical

--- src/components/atoms/cards/CardProject.tsx
+++ src/components/atoms/cards/CardProject.tsx
@@ -4,13 +4,16 @@ import { Box, Typography } from '@mui/material';
 import Link from 'next/link';
 import Image from 'next/image';
 import { ArrowIcon } from '..';
-import { base, gray } from '@/styles/colors';
+import { base, gray, primary } from '@/styles/colors';
+import { BreakPoints, useBreakpoint } from '@/hooks';
 
 interface ICardProjectProps {
   item: ProjectType;
 }
 
 export const CardProject: React.FunctionComponent<ICardProjectProps> = ({ item }) => {
+  const isMobile = useBreakpoint(BreakPoints.MD);
+
   return (
     <Link href={item.href}>
       <Box
@@ -19,15 +22,27 @@ export const CardProject: React.FunctionComponent<ICardProjectProps> = ({ item }
         p="12px"
         borderRadius="8px"
         bgcolor={base.white}
-        boxShadow="0px 10px 47px 0px #EFEFEF"
+        sx={{
+          '&:hover': {
+            boxShadow: '0px 10px 47px 0px #EFEFEF',
+            '& .button-link': {
+              color: primary[500],
+              transition: 'color 0.3s ease',
+            },
+            '& .arrow-icon path': {
+              stroke: primary[500],
+              transition: 'stroke 0.3s ease',
+            },
+          },
+        }}
       >
         <Image
-          src={item.imgSrc}
+          src={item.quote.imgSrc}
           alt={item.name}
           style={{ width: '100%', height: '100%', borderRadius: '8px' }}
         />
         <Box display="flex" flexDirection="column" gap="8px">
-          <Typography variant="fs18" fontWeight={600} color={base.black}>
+          <Typography variant={isMobile ? 'fs16' : 'fs18'} fontWeight={600} color={base.black}>
             {item.name}
           </Typography>
           <Typography
@@ -40,7 +55,7 @@ export const CardProject: React.FunctionComponent<ICardProjectProps> = ({ item }
             {item.content}
           </Typography>
           <Box display="flex" justifyContent="space-between" alignItems="center">
-            <Typography variant="fs14" fontWeight={600} color={gray[400]}>
+            <Typography variant="fs14" fontWeight={600} color={gray[400]} className="button-link">
               Read more
             </Typography>
             <ArrowIcon color={gray[400]} />
