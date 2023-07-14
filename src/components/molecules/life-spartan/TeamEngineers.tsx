449697154865820db18de35935a63e15097ddbf7
Tuan Nguyen <tuan.nguyen@c0x12c.com>
2023-07-14 20:51:31 +0700

    feat: implement drag scrolling

--- src/components/molecules/life-spartan/TeamEngineers.tsx
+++ src/components/molecules/life-spartan/TeamEngineers.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import { Box, styled } from '@mui/material';
+import { Box } from '@mui/material';
 import { Container, SubTitle, Title } from '@/components/atoms';
 import { BreakPoints, useBreakpoint } from '@/hooks';
 import Image from 'next/image';
-import Slider, { Settings } from 'react-slick';
 
 import team1 from '@/assets/images/life-spartan/our-team-1.png';
 import team2 from '@/assets/images/life-spartan/our-team-2.png';
@@ -11,7 +10,7 @@ import team3 from '@/assets/images/life-spartan/our-team-3.png';
 import team4 from '@/assets/images/life-spartan/our-team-4.png';
 import team5 from '@/assets/images/life-spartan/our-team-5.png';
 import team6 from '@/assets/images/life-spartan/our-team-6.png';
-import team7 from '@/assets/images/life-spartan/our-team-7.png';
+// import team7 from '@/assets/images/life-spartan/our-team-7.png';
 import team8 from '@/assets/images/life-spartan/our-team-8.png';
 import team9 from '@/assets/images/life-spartan/our-team-9.png';
 import team10 from '@/assets/images/life-spartan/our-team-10.png';
@@ -20,20 +19,7 @@ import { gray } from '@/styles/colors';
 
 interface ITeamEngineersProps {}
 
-const CustomInner = styled(Box)(({ theme }) => ({
-  '& .slick-list': {
-    overflow: 'visible',
-    margin: '0 -24px',
-  },
-  [theme.breakpoints.down('md')]: {
-    '& .slick-list': {
-      overflow: 'hidden',
-    },
-  },
-}));
-
 export const TeamEngineers: React.FunctionComponent<ITeamEngineersProps> = () => {
-  const isTablet = useBreakpoint(BreakPoints.LG);
   const isMobile = useBreakpoint(BreakPoints.MD);
 
   const dataItems = [
@@ -43,57 +29,78 @@ export const TeamEngineers: React.FunctionComponent<ITeamEngineersProps> = () =>
     { id: 'team-4', img: team4 },
     { id: 'team-5', img: team5 },
     { id: 'team-6', img: team6 },
-    { id: 'team-7', img: team7 },
+    // { id: 'team-7', img: team7 },
     { id: 'team-8', img: team8 },
     { id: 'team-9', img: team9 },
     { id: 'team-10', img: team10 },
   ];
 
-  const settings: Settings = {
-    dots: false,
-    arrows: false,
-    infinite: true,
-    autoplay: false,
-    slidesToShow: 2,
-    centerMode: true,
-    variableWidth: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-    ],
-  };
-
   const renderItem = dataItems.map((item) => {
     return (
-      <Box key={item.id} mx="24px">
+      <Box key={item.id}>
         <Image src={item.img} alt={item.id} style={{ height: 'auto' }} />
       </Box>
     );
   });
 
+  React.useEffect(() => {
+    const slider = document.querySelector('.team-items') as HTMLElement;
+    let startX = 0;
+    let isDragging = false;
+    let scrollLeft = 0;
+
+    if (slider && slider !== null) {
+      slider.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+      });
+      slider.addEventListener('mouseleave', () => {
+        isDragging = false;
+      });
+      slider.addEventListener('mouseup', () => {
+        isDragging = false;
+        slider.classList.remove('active');
+      });
+      slider.addEventListener('mousemove', (e) => {
+        if (!isDragging) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 1.5; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+        console.log(walk);
+      });
+    }
+  }, []);
+
   return (
-    <Box py={isTablet ? '40px' : '100px'} bgcolor={gray[50]}>
+    <Box py={isMobile ? '40px' : '100px'} bgcolor={gray[50]}>
       <Container>
         <Box display={'flex'} flexDirection={'column'} gap={'24px'} mb={'41px'}>
           <SubTitle text="Our team" data-aos="fade-up" data-aos-delay="200" />
           <Title text="Life at Spartan" data-aos="fade-up" data-aos-delay="300" />
         </Box>
       </Container>
-      <CustomInner>
-        <Slider {...settings}>{renderItem}</Slider>
-      </CustomInner>
+      <Box
+        className="team-items"
+        display="flex"
+        gap="24px"
+        px="24px"
+        sx={{
+          width: '100%',
+          overflowX: 'visible',
+          overflowY: 'hidden',
+          whiteSpace: 'nowrap',
+          userSelect: 'none',
+          cursor: 'pointer',
+          scrollbarWidth: 'none',
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+        }}
+      >
+        {renderItem}
+      </Box>
     </Box>
   );
 };
