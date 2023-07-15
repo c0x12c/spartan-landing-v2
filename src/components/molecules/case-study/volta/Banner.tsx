5f480c1376fc4ede09487dc8a434ff2d531fd1c2
volodymyr-kyrylenko-c0x12c <134924457+volodymyr-kyrylenko-c0x12c@users.noreply.github.com>
2023-07-15 20:12:22 +0300

    fix: using resued components

--- src/components/molecules/case-study/volta/Banner.tsx
+++ src/components/molecules/case-study/volta/Banner.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
-import * as colors from '@/styles/colors';
 import Image from 'next/image';
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import bg from '@/assets/images/case-volta/banner.png';
 import bgSp from '@/assets/images/case-volta/banner-sp.png';
 import appIcon from '@/assets/images/case-volta/app-icon.png';
-import { Banner, Container } from '@/components/atoms';
-import { base } from '@/styles/colors';
+import { Banner, BodyText, Container, MainTitle } from '@/components/atoms';
 import { BreakPoints, useBreakpoint } from '@/hooks';
 import BannerImageMD from '../../../../assets/images/case-volta/banner-image.svg';
+import { base } from '@/styles/colors';
 
 interface ICaseStudyVoltaBannerProps {}
 
@@ -41,32 +40,23 @@ export const CaseStudyVoltaBanner: React.FunctionComponent<ICaseStudyVoltaBanner
               <Grid container>
                 <Grid item xs={12} md={6}>
                   <Image src={appIcon} alt="appIcon" />
-                  <Typography
-                    variant="fs64"
-                    fontSize={{ xs: '36px', lg: '64px' }}
-                    color={base.white}
+                  <MainTitle
+                    text="Volta Wallet"
+                    sx={{ my: '24px' }}
                     data-aos="fade-up"
                     data-aos-delay="200"
-                    component="h1"
-                    my="24px"
-                  >
-                    Volta Wallet
-                  </Typography>
-                  <Typography
-                    variant={'fs18'}
-                    fontSize={{ xs: '14px', lg: '18px' }}
-                    color={colors.base.white}
-                    maxWidth="538px"
-                    data-aos="fade-up"
-                    data-aos-delay="300"
-                  >
-                    Volta Wallet is a unique crypto wallet application developed by Spartan, an IT
+                  />
+                  <BodyText
+                    sx={{ maxWidth: '538px', color: base.white }}
+                    text="Volta Wallet is a unique crypto wallet application developed by Spartan, an IT
                     consulting company. The mission of Volta Wallet is to provide effortless Web3
                     solutions, focusing on delivering exceptional functionality, security, and user
                     experience in the realm of Web3 technologies. The goal is to empower users with
                     seamless access to the benefits of the decentralized web, driving meaningful
-                    change and creating a connected world.
-                  </Typography>
+                    change and creating a connected world."
+                    data-aos="fade-up"
+                    data-aos-delay="300"
+                  />
                 </Grid>
               </Grid>
             </Container>
