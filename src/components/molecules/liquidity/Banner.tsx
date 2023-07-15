1dd9dfd2b6f923f24d5c5a09859424f919572f84
volodymyr-kyrylenko-c0x12c <134924457+volodymyr-kyrylenko-c0x12c@users.noreply.github.com>
2023-07-15 20:14:14 +0300

    fix: reset changes in liquidity(pushed in wrong branch)

--- src/components/molecules/liquidity/Banner.tsx
+++ src/components/molecules/liquidity/Banner.tsx
@@ -1,12 +1,11 @@
 import { Banner } from '@/components/atoms/Banner';
 import BG from '@/assets/images/case-liquidity/Background.svg';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import Image from 'next/image';
 import BannerImage from '@/assets/images/case-liquidity/image-banner.svg';
 import { Container } from '@/components/atoms/common/Container';
+import { base } from '@/styles/colors';
 import { BreakPoints, useBreakpoint } from '@/hooks';
-import { BodyText, MainTitle } from '@/components/atoms';
-import BannerImageMD from '../../../assets/images/case-volta/banner-image.svg';
 
 const BannerLiquidity = () => {
   const isMobile = useBreakpoint(BreakPoints.SM);
@@ -22,16 +21,22 @@ const BannerLiquidity = () => {
             <Box
               display={'flex'}
               flexDirection={'column'}
-              gap={'32px'}
+              gap={{ lg: '32px', xs: '24px' }}
               justifyContent={'center'}
               data-aos="fade-right"
               data-aos-delay="200"
             >
-              <MainTitle text={`Liquidity ${(<br />)} Financial`} />
-              <BodyText
-                text="Transforming Banking and Spending for the New Generation"
-                sx={{ maxWidth: '488px' }}
-              />
+              <Typography variant="fs64" fontSize={{ xs: '36px', lg: '64px' }} color={base.white}>
+                Liquidity <br /> Financial
+              </Typography>
+              <Typography
+                variant="fs24"
+                fontSize={{ xs: '18px', lg: '24px' }}
+                color={base.white}
+                maxWidth={'488px'}
+              >
+                Transforming Banking and Spending for the New Generation
+              </Typography>
             </Box>
             {!isMobile && (
               <Image
