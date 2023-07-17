1402cc0402dc517610f6120370e6929e9646efe6
Tuan Nguyen <tuan.nguyen@c0x12c.com>
2023-07-17 09:30:18 +0700

    chore: scroll to the top when navigate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,8 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '@/styles/animations.css';
 import '@/styles/index.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export type MyAppProps = AppProps;
 
@@ -27,6 +29,33 @@ const MyApp: React.FC<MyAppProps> = (props) => {
       duration: 800,
     });
   }, []);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const routeChangeStart = () => {
+      console.log('routeChangeStart');
+    };
+    const routeChangeComplete = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    };
+    const routeChangeError = (e: Error) => {
+      console.error('routeChangeError', e);
+    };
+    router.events.on('routeChangeStart', routeChangeStart);
+    router.events.on('routeChangeComplete', routeChangeComplete);
+    router.events.on('routeChangeError', routeChangeError);
+    // from the event with the `off` method:
+    return () => {
+      router.events.off('routeChangeStart', routeChangeStart);
+      router.events.off('routeChangeComplete', routeChangeComplete);
+      router.events.off('routeChangeError', routeChangeError);
+    };
+  }, [router]);
 
   return (
     <ThemeProvider theme={theme}>
