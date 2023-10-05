0e40982435f692fdd985bc907b9f456a31a3b928
spartan-tuannguyen <tuan.nguyen@c0x12c.com>
2023-10-05 11:29:01 +0700

    chore: add GA tracking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import { Html, Head, Main, NextScript } from 'next/document';
 import { GlobalHead } from '@/components/atoms';
 
@@ -6,6 +7,17 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <GlobalHead />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GSKNE1X103"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GSKNE1X103');
+          `,
+          }}
+        ></script>
       </Head>
       <body>
         <Main />
